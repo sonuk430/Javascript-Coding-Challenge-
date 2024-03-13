f449c7d1893494fe26538d29c74b0f9bc4669648
Sonu Kumar <kumarsonuk430@gmail.com>
2024-03-14 02:05:49 +0530

    sumOfNaturalNumber

--- index.js
+++ index.js
@@ -36,4 +36,35 @@ const FleteArr = arr.reduce(
 
 // console.log(FleteArr);
 
+// 1. Write a JavaScript function to check whether an `input` is an array or not.
+
+function isarr(input) {
+  if (toString.call(input) === "[object Array]") {
+    return true;
+    return false;
+  }
+}
+// console.log(isarr([1,2]));
+
+
+//  Question 1: Sum of all natural numbers from 1 to n
+// Question 2: Sum of digits of a number
+// Question 3: Count the number of digits of a number
+// Question 4: Check if a number is palindrome
+// Question 5: Find nth Fibonacci number
+// Question 6: Missing Number in an Array
+// Question 7: Count Odd Numbers in an Interval Range
+// Question 8: Power of Two
+// Question 9: Find Square root of a Number
+
+
+function sumOfNaturalNumber(num){
+  let sum = 0;
+  for(let i = 0; i<=num;i++){
+    sum = sum + 1;
+  }
+  return sum;
+}
+
+
 
